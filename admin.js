08d595642c6822a547e44698e688e5ee2c691bfc
gabimolocea <gabimolocea@gmail.com>
2025-04-17 16:50:40 +0300

    added base64 key

--- admin.js
+++ admin.js
@@ -1,10 +1,17 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./key.json'); // Path to your key file
+import { initializeApp, cert, getApps } from 'firebase-admin/app';
 
-// Initialize the Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://ai-design-contests-default-rtdb.europe-west1.firebasedatabase.app", // Replace with your Firebase project URL
-});
+// Check if Firebase Admin SDK is already initialized
+if (!getApps().length) {
+  // Decode the Base64 string from the environment variable
+  const keyJson = Buffer.from(process.env.GOOGLE_CLOUD_KEY_JSON || '', 'base64').toString('utf-8');
 
-module.exports = admin;
+  // Parse the JSON string
+  const serviceAccount = JSON.parse(keyJson);
+
+  // Initialize Firebase Admin SDK
+  initializeApp({
+    credential: cert(serviceAccount),
+  });
+}
+
+export default initializeApp;
